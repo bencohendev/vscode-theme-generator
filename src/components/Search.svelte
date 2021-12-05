59bead92ebbd38296c65bf33688173adb3afb3bb
bencohendev <ben@bencohen.dev>
2021-12-05 10:23:52 -0500

    search + results + more specific term neede

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -6,6 +6,8 @@
   let options = {
       includeScore: true,
       useExtendedSearch: true,
+      includeMatches: true,
+      threshold: .4,
       keys: ['tags', 'scopes']
     }
   let colorInfoSearchable =[]
@@ -16,9 +18,19 @@
     const fuse = new Fuse(colorInfoSearchable, options)
     searchReturn = fuse.search(value)
     if (searchReturn.length > 0) {
-      searchReturn = searchReturn.map((result)=>result.item.name)
+      searchReturn = searchReturn.map((result)=>{
+        const formattedResult = {
+          name: result.item.name,
+          matches: result.matches,
+          showMatches: false
+        }
+        return formattedResult
+      })
     } 
     showResults = true
+  }
+  const showMatches = (e,i) => {
+    searchReturn[i].showMatches = !searchReturn[i].showMatches
   }
   onMount(()=> {
     Object.values(colorInfo).forEach((colors)=> {
@@ -32,15 +44,28 @@
 </script>
 
 <div>
-  <div>Search for a tag or scope to find what color is associated with it</div>
+  <div class="search-text">Search for a tag or scope to find what color is associated with it</div>
   <input type="text" name="search" id="" bind:value>
   <button on:click={()=>handleSearch()}>Search</button>
 </div>
 {#if showResults}
   <h4>Try looking at the following colors</h4>
-<ul>  
-  {#each searchReturn as result}
-    <li>{result}</li>
-  {/each}
-</ul>
+  <ul>  
+    {#each searchReturn as result, i}
+      <li on:click={(e)=>showMatches(e,i)}>{result.name}</li>
+      {#if result.showMatches == true}
+        <ul>
+          {#each result.matches as match}
+            <li>{match.key}: {match.value}</li>
+          {/each}
+        </ul>
+      {/if}
+    {/each}
+  </ul>
 {/if}
+
+<style>
+  .search-text {
+    margin-bottom: 1rem;
+  }
+</style>
