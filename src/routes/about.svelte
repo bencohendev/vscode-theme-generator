d92e94d8430d0069b13c6f4b33888b471c45f945
bencohendev <ben@bencohen.dev>
2021-12-11 23:38:29 -0500

    extra about info

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,28 +1,58 @@
-<script>
-	import Header from '../components/Header.svelte';
-</script>
-
-		<h3>About</h3>
+<h3>About</h3>
+<p>
+	This project is an attempt to make theming Vs Code easier and more
+	approachable. One should be able to get a general idea of the uses of each
+	color from the description and for more in depth information, a list of all
+	tags and scopes to which each color is applied are supplied. The basic
+	structure of this theme is largely inspired by the ever popular Dracula theme,
+	with some modifications. And for braver users, there are plenty of advanced
+	colors you can set to truly trick out your theme. If there is a scope or tag
+	you'd like to more easily target, just
+	<a href="mailto:ben@bencohen.dev">let me know</a> and I'll add it.
+</p>
+<div>
+	Some basic info about VS Code themes:
+	<div>
+		There are primarily two ways you can change colors in VS Code. You can
+		either add them to your settings.json or you can create a completely new
+		theme with the json file you download on theme generation. If you end up
+		publishing a theme that you generated here, I'd really appreciate a
+		shoutout! Here's the documentation on
+		<a
+			href="https://code.visualstudio.com/docs/getstarted/themes#_creating-your-own-color-theme"
+			>theme generation</a
+		>
+	</div>
+	<div>
+		VS Code splits its keys into two groups: <strong>tags</strong> and
+		<strong>scopes</strong>.
 		<p>
-			This project is an attempt to make theming Vs Code easier and more approachable. One should be
-			able to get a general idea of the uses of each color from the description and for more in
-			depth information, a list of all tags and scopes to which each color is applied are supplied.
+			Tags generally set colors for everything other than explicitly code
+			related colors.
+
+			<strong>Scopes</strong> are for code specific colors. 
+
+			You'll find a list of scopes and tags associated with each color you can set. Also, if
+			you know what scope or tag you want to change or want to change the color of something
+			specific, but don't know what color set it is associated with, just search it!
 		</p>
-		<h3>Resources</h3>
-		<ul>
-			<li>
-				<a href="https://code.visualstudio.com/docs/getstarted/themes">Vs Code Basic Theming Info</a
-				>
-			</li>
-			<li>
-				<a href="https://code.visualstudio.com/api/references/theme-color"
-					>Vs Code Theme Colors Reference</a
-				>
-			</li>
-		</ul>
+	</div>
+</div>
+<h3>Resources</h3>
+<ul>
+	<li>
+		<a href="https://code.visualstudio.com/docs/getstarted/themes"
+			>VS Code Basic Theming Info</a
+		>
+	</li>
+	<li>
+		<a href="https://code.visualstudio.com/api/references/theme-color"
+			>VS Code Theme Colors Reference</a
+		>
+	</li>
+</ul>
 
 <style>
-
 	a {
 		text-decoration: none;
 	}
