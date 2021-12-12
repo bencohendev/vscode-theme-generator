090238a03840b933eec5ee6c27971d028352c982
bencohendev <ben@bencohen.dev>
2021-12-11 21:17:03 -0500

    sitemapjs

---
+++ src/routes/sitemap.xml.js
@@ -0,0 +1,16 @@
+export async function get() {
+  try {
+    const data = fs.readFileSync('sitemap.xml', 'utf8');
+
+    return {
+      headers: {
+        'Content-Type': 'application/xml'
+      },
+      body: data
+    };
+  } catch (e) {
+    return {
+      body: 'Error'
+    };
+  }
+}
